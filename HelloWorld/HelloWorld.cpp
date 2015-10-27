c81d26abd777364e0d6727c19f2abdec6ed2912c
Mikko <mikko.t.kukkola@gmail.com>
2015-10-27 13:58:14 +0200

    HelloWorld functionality done

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,18 @@
+#include "HelloWorld.h"
+#include <iostream>
+using namespace std;
+
+
+HelloWorld::HelloWorld()
+{
+}
+
+
+HelloWorld::~HelloWorld()
+{
+}
+
+void HelloWorld::SayHello()
+{
+	cout << "Hello World!" << endl;
+}
